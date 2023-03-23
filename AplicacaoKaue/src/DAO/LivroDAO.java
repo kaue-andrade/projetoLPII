7c100eb0b28ed038ec32bdd21ec9e8c4bbbb282e
Kauê Andrade <91803383+kaue-andrade@users.noreply.github.com>
2023-03-23 18:50:30 -0300

    Java
    Java

---
+++ AplicacaoKaue/src/DAO/LivroDAO.java
@@ -0,0 +1,95 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import DTO.Livro;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LivroDAO {
+
+    private GerenciadorBD bd;
+
+    public LivroDAO() {
+        bd = new GerenciadorBD();
+    }
+
+    public void cadastrar(Livro livro) throws SQLException {
+        try (Connection conexao = bd.conectar(); 
+             PreparedStatement comando = conexao.prepareStatement(
+             "INSERT INTO livro (titulo, ano, paginas, autores, preco) VALUES (?, ?, ?, ?, ?)")
+            ) 
+        {
+            comando.setString(1, livro.getTitulo());
+            comando.setInt(2, livro.getAno());
+            comando.setInt(3, livro.getPaginas());
+            comando.setString(4, livro.getAutores());
+            comando.setDouble(5, livro.getPreco());
+            
+            comando.executeUpdate();
+        }
+    }
+
+    public List<Livro> listar() throws SQLException {
+
+        List<Livro> listaDeLivros = new ArrayList<>();
+
+        try (Connection conexao = bd.conectar(); 
+             PreparedStatement comando = conexao.prepareStatement(
+             "SELECT ID, titulo, ano, paginas, autores, preco FROM livro");
+             ResultSet tabela = comando.executeQuery()
+            )
+        {
+            while (tabela.next()) {
+                Livro livro = new Livro();
+
+                livro.setId(tabela.getInt("id"));
+                livro.setTitulo(tabela.getString("titulo"));
+                livro.setAno(tabela.getInt("ano"));
+                livro.setPaginas(tabela.getInt("paginas"));
+                livro.setAutores(tabela.getString("autores"));
+                livro.setPreco(tabela.getDouble("preco"));
+
+                listaDeLivros.add(livro);
+            }
+        }
+
+        return listaDeLivros;
+    }
+    
+    public void excluir(Livro livro) throws SQLException {
+        try (Connection conexao = bd.conectar();
+             PreparedStatement comando = conexao.prepareStatement (
+                     "DELETE FROM livro WHERE id = ?")
+        )
+            
+        {    
+        comando.setInt(1, livro.getId());
+        
+        comando.executeUpdate();
+        }
+    }
+    
+    public void alterar(Livro livro) throws SQLException {
+        try(Connection conexao = bd.conectar();
+           PreparedStatement comando = conexao.prepareStatement(
+           "UPDATE livro SET titulo = ?, ano = ?, paginas = ?, autores = ?, preco = ? WHERE id = ?")
+           )   
+                
+           {
+               comando.setString(1, livro.getTitulo());
+               comando.setInt(2, livro.getAno());
+               comando.setInt(3, livro.getPaginas());
+               comando.setString(4, livro.getAutores());
+               comando.setDouble(5, livro.getPreco());
+               comando.setInt(4, livro.getId());
+               
+               comando.executeUpdate();
+               
+               
+           }
+    
+    }
+}
---
+++ AplicacaoKaue/src/DTO/Livro.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DTO;
+
+/**
+ *
+ * @author Kauê
+ */
+public class Livro {
+    private int id;
+    private String titulo;
+    private int ano;
+    private int paginas;
+    private String autores;
+    private double preco;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the ano
+     */
+    public int getAno() {
+        return ano;
+    }
+
+    /**
+     * @param ano the ano to set
+     */
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    /**
+     * @return the paginas
+     */
+    public int getPaginas() {
+        return paginas;
+    }
+
+    /**
+     * @param paginas the paginas to set
+     */
+    public void setPaginas(int paginas) {
+        this.paginas = paginas;
+    }
+
+    /**
+     * @return the autores
+     */
+    public String getAutores() {
+        return autores;
+    }
+
+    /**
+     * @param autores the autores to set
+     */
+    public void setAutores(String autores) {
+        this.autores = autores;
+    }
+
+    /**
+     * @return the preco
+     */
+    public double getPreco() {
+        return preco;
+    }
+
+    /**
+     * @param preco the preco to set
+     */
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    
+    
+    
+}
